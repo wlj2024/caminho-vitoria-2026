2aa0cdc5ee919b0d3e44ea68663cc2d6155d5d1c
wlj2024 <puppimleo@gmail.com>
2025-06-25 09:55:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/caminho-vitoria-2026/',
-  plugins: [react()]
+  plugins: [react()],
+  base: './',
 })
