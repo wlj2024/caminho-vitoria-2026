e25622e565018ee4a6704f55e8d1a4a61a29e33a
wlj2024 <puppimleo@gmail.com>
2025-06-25 10:00:51 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CaminhoVitoria2026 from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CaminhoVitoria2026 />
+  </React.StrictMode>,
+)
